716222b3364b8395a45eecddd671ac57e960ba68
Benbinbin <benthomsonbin@gmail.com>
2021-07-08 21:41:50 +0800

    change home icon.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -88,7 +88,7 @@ module.exports = {
   define: {
     __BASE__: "/blog-web/",
     __HOME_PAGE_TITLE__: "WEB",
-    __HOME_PAGE_ICON__: "home_icon.svg",
+    __HOME_PAGE_ICON__: "home_icon.png",
     __HOME_DESCRIPTION__: "这是我的<a href='https://benbinbin.github.io/tech-blog/' target='_blank' class='hover:text-blue-600 underline font-bold transition-colors'>技术部落格</a>之一，主要内容是与前后端开发相关的笔记。",
     __HOME_PAGE_COLOR__: '#60A5FA',
     __AVATAR__: 'avatar.png',
